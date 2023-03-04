2c783d1257cce7bd62c67ffaaac08e76987a3de8
Villetelle Nicolas <nicolas.villetelle@hotmail.fr>
2023-03-04 19:07:15 +0100

    add button show more

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,19 @@ import {getImage, getId} from "@/utils/pokemonService";
 const pokemons = ref<Pokemon[]>([]);
 let filteredPokemons = ref<Pokemon[]>([]);
 let input = ref("");
+let limit = ref(9);
 
 const filteredListPokemons = (search: string): Pokemon[] => {
   if ("" === search){
-    return [];
+    return filteredPokemons.value = pokemons.value;
   }
   return pokemons.value.filter((search) =>
       search.name.toLowerCase().includes(input.value.toLowerCase())
   );
+}
+
+const showMore = () => {
+  limit.value += 9;
 }
 
 watch(input, (value) => {
@@ -29,21 +34,32 @@ onMounted(() => {
    axios.get<PokemonResponseDto>('https://pokeapi.co/api/v2/pokemon/?limit=151')
        .then((response) => {
          pokemons.value = response.data.results;
+         filteredPokemons.value = pokemons.value;
        })
 })
 
 </script>
 
 <template>
-  <h1>Pokedex</h1>
-  <input type="text" v-model="input" placeholder="Search pokemon">
+
+  <div class="header">
+    <h1>Pokedex</h1>
+    <input type="text" v-model="input" placeholder="Search pokemon">
+  </div>
+
   <div class="container">
-    <div class="card" v-for="(pokemon) in filteredPokemons" :key="pokemon.name">
+    <div class="card" v-for="(pokemon) in filteredPokemons.slice(0, limit)" :key="pokemon.name">
       <h3>{{ pokemon.name }}</h3>
       <img :src="getImage(getId(pokemon.url))" :alt="pokemon.name">
     </div>
   </div>
-  <Test></Test>
+
+  <div class="show-more">
+    <button class="btn" v-if="filteredPokemons.length > limit" @click="showMore">
+      Show More
+    </button>
+  </div>
+
 </template>
 
 <style scoped>
